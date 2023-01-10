4cd3d064ed1796d5aa5bc43cb90918b4662e86b9
Alexey Belkov <lexabelco2013@yandex.ru>
2023-01-11 01:54:58 +0500

    feat: подготовить стартовый шаблон проекта

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className='app__container'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
