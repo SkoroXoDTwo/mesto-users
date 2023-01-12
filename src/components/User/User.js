75685fb1b6d1a456165d8f72ca6e3a1420fb3334
Alexey Belkov <lexabelco2013@yandex.ru>
2023-01-12 22:33:57 +0500

    feat: создать компоненты user и users

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,10 +1,18 @@
 import "./User.scss";
 
-function User() {
+function User({ name, about, avatar }) {
   return (
     <li>
       <article className="user">
-        Карточка
+        <img
+          className="user__avatar"
+          src={avatar}
+          alt={`Аватар пользователя ${name}`}
+        />
+        <div className="user__info">
+          <h2 className="user__name">{name}</h2>
+          <p className="user__about">{about}</p>
+        </div>
       </article>
     </li>
   );
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,13 +1,38 @@
 import "./Users.scss";
-import User from "../User/User"
+import User from "../User/User";
 
 function Users() {
+  const users = [
+    {
+      name: "Alex",
+      about: "Учусь на Frontend-разработчика fdf fdf  df d fdfdf df d fdfdf fdf  df d fdfdf fdf  df d fdfdf fdf  df d fdfdf fdf  df d fdfdf fdf  df d fdfdf ",
+      avatar: "https://www.amworld.ru/upload/000/u3/3/4/aaeef86e.jpg",
+      _id: 1,
+    },
+    {
+      name: "Alex",
+      about: "Учусь на Frontend-разработчика",
+      avatar: "https://www.amworld.ru/upload/000/u3/3/4/aaeef86e.jpg",
+      _id: 2,
+    },
+    {
+      name: "Alex",
+      about: "Учусь на Frontend-разработчика",
+      avatar: "https://www.amworld.ru/upload/000/u3/3/4/aaeef86e.jpg",
+      _id: 3,
+    },
+  ];
+
   return (
     <ul className="users">
-      <User />
-      <User />
-      <User />
-      <User />
+      {users.map((user) => (
+        <User
+          key={user._id}
+          name={user.name}
+          about={user.about}
+          avatar={user.avatar}
+        />
+      ))}
     </ul>
   );
 }
